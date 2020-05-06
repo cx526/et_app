98bf52d1488d70a35ca2aa7be8364c1693621cb0
Decade <sitting2002@126.com>
2020-05-06 14:27:33 +0800

    mod file

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,7 +1,16 @@
 <template>
 	<view>
-		hello world
-		bye world
+		<view class="search-box">
+			<input class="search-input" type="text" value="输入关键字搜索" disabled />
+		</view>
+		
+		<view>
+			
+		</view>
+		
+		<view>
+			
+		</view>
 	</view>
 </template>
 
@@ -22,5 +31,11 @@
 </script>
 
 <style scoped>
+.search-box {
+	
+}
 
+.search-input {
+	
+}
 </style>
