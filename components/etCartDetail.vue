2a2126c36cb201256c23ddca97b96c5e8f095575
hojohnfit <your user email>
2020-05-14 22:00:33 +0800

    add pages and image

---
+++ components/etCartDetail.vue
@@ -0,0 +1,114 @@
+<template>
+	<view class="content" >
+		<view class="check-position">
+			<checkbox value="cb" checked="true" />
+		</view>
+		
+		<view class="img-position">
+			<image :src="imgSrc" class="img-content"></image>
+		</view>
+		
+		<view class="content-position">
+			<view class="title-position">
+				<text>{{title}}</text>
+			</view>
+			
+			<view class="white-space" style="height: 20upx"></view>
+			
+			<view class="status-position">
+				<text>{{status}}</text>
+			</view>	
+			
+			<view class="white-space" style="height: 90upx"></view>
+			
+			<view class="corn-position">
+				<text>借阅币：{{corn}}</text>
+			</view>
+		</view>
+		
+		<view class="last-position">
+			<view class="rubbish-position">
+				<image class="rubbish-img" src="../static/cart/rubbish.png"></image>
+			</view>
+			
+			<view class="count-position">
+				<text>X {{count}}</text>
+			</view>	
+		</view>
+	</view>
+</template>
+
+<script>
+export default {
+	props: {
+		imgSrc: String,
+		title: String,
+		status: String,
+		corn: String,
+		count: String
+		
+	},
+	methods: {
+	
+	}
+}
+</script>
+
+<style>
+.content {
+	width: 100%;
+	display: flex;
+	flex-direction: row;
+	justify-items: space-around;
+	align-items: center;
+	text-align: center;
+	border-radius: 50upx;
+	border: 1upx solid rgba(179,179,179,0.3);
+	box-shadow: 1upx 1upx 1upx 1upx rgba(179,179,179,0.5);
+}
+.img-content {
+	width: 250upx;
+	height: 250upx;
+}
+.content-position {
+	display: flex;
+	flex-direction: column;
+	justify-items: space-between;
+	align-items: flex-start;
+	height: 250upx;
+	padding-top: 50upx;
+}
+.title-position {
+	font-size: 30upx;
+	font-weight: bold;
+}
+.status-position {
+	font-size: 20upx;
+	color: #6A6A6A;
+}
+.corn-position {
+	font-size: 24upx;
+	color: #549EAB;
+	font-weight: bold;
+}
+.rubbish-img {
+	width: 30upx;
+	height: 30upx;
+}
+.count-position {
+	background-color: #E6E6E6;
+	color:  #6A6A6A;
+	border-radius: 20upx;
+	padding: 10upx;
+	font-size: 20upx;
+}
+.last-position {
+	height: 250upx;
+	display: flex;
+	flex-direction: column;
+	justify-content: space-between;
+	padding-top: 30upx;
+	padding-left: 30upx;
+	padding-bottom: 30upx;
+}
+</style>
--- pages/cart/cart.vue
+++ pages/cart/cart.vue
@@ -1,8 +1,44 @@
 <template>
+	<view class="content">
+		<view class="cat-detail-position">
+			<et-cart-detail :imgSrc="imgSrc" :title="title" :status="status" :corn="corn" :count="count"></et-cart-detail>
+		</view>
+	</view>
 </template>
 
 <script>
+import etCartDetail from '../../components/etCartDetail.vue'
+
+export default {
+	components: {
+		etCartDetail
+	},
+	data() {
+		return {
+			imgSrc: "../static/cart/oldMan.png",
+			title: "巴巴和圣诞老人",
+			status: "可借阅",
+			corn: "30",
+			count: "9"
+		}
+	},
+	onLoad() {
+		
+	},
+	methods: {
+		
+	}
+}
 </script>
 
 <style>
+.content {
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+}
+.cat-detail-position {
+	width: 90%;
+}
 </style>
