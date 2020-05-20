65cc3cb3949037f2e4461c922f2cea76a9b8d972
Decade <sitting2002@126.com>
2020-05-20 14:29:46 +0800

    mod complateInfo

--- pages/guide/complateInfo.vue
+++ pages/guide/complateInfo.vue
@@ -1,46 +1,25 @@
 <template>
 	<view class="content">
 		<view class="content-position" style="background-image: url(../../static/guide/paper.png);">
-			<!-- 姓名 -->
-			<!-- 
-			<view class="in-position">
-				<text class="title-content">姓名</text>
-				<input class="input-size input-style" type="text" value="" />
-				
-			</view>
-			
-			<view class="in-position">
-				<text class="title-content">性别</text>
-				<view class="input-size">
-					<picker @change="bindSexChange" :value="sexIndex" :range="sex">
-						<view class="input-style">{{sex[sexIndex]}}</view>
-					</picker>
-				</view>
-				
-			</view>
-			
-			<view class="in-position">
-				<text class="title-content">出生时间</text>
-				<view class="input-size">
-					<picker mode="date" :value="date" :start="startDate" :end="endDate" @change="bindDateChange">
-						<view class="input-style">{{date}}</view>
-					</picker>
-				</view>
-				
-			</view> 
-			-->
 			<view class="sub-content">
 				<view class="in-position">
 					<text class="title-content">所在地</text>
-					<picker class="input-size" @change="bindDistrictChange" :value="districtIndex" :range="district">
-						<view class="input-style">{{district[districtIndex]}}</view>
+					<picker 
+						class="picker-input" 
+						mode="multiSelector" 
+						@change="regionChange"
+						@columnchange="regionColChange"
+						:value="regionResult" 
+						:range="region"
+					>
+						<view class="input-style">{{ finalRegionString.length === 0 ? '选择幼儿园所在地': finalRegionString }}</view>
 					</picker>
 					<image src="../../static/guide/arrow.png" class="arrow"></image>
 				</view>
 				
 				<view class="in-position">
 					<text class="title-content">幼儿园</text>
-					<picker class="input-size" @change="bindKindergartenChange" :value="kindergartenIndex" :range="kindergarten">
+					<picker class="picker-input" @change="bindKindergartenChange" :value="kindergartenIndex" :range="kindergarten">
 						<view class="input-style">{{kindergarten[kindergartenIndex]}}</view>
 					</picker>
 					<image src="../../static/guide/arrow.png" class="arrow"></image>
@@ -48,7 +27,7 @@
 				
 				<view class="in-position">
 					<text class="title-content">所在年级</text>
-					<picker class="input-size" @change="bindGradeChange" :value="gradeIndex" :range="grade">
+					<picker class="picker-input" @change="bindGradeChange" :value="gradeIndex" :range="grade">
 						<view class="input-style">{{grade[gradeIndex]}}</view>
 					</picker>
 					<image src="../../static/guide/arrow.png" class="arrow"></image>
@@ -56,7 +35,7 @@
 				
 				<view class="in-position">
 					<text class="title-content">所在班级</text>
-					<picker class="input-size" @change="bindTeamChange" :value="teamIndex" :range="team">
+					<picker class="picker-input" @change="bindTeamChange" :value="teamIndex" :range="team">
 						<view class="input-style">{{team[teamIndex]}}</view>
 					</picker>
 					<image src="../../static/guide/arrow.png" class="arrow"></image>
@@ -76,17 +55,35 @@
 </template>
 
 <script>
+import regionData from '../../common/regionData.js'
+
 export default {
     data() {
-		const currentDate = this.getDate({
-			format: true
-		})
         return {
-			sexIndex: 0,
-			sex: ['男孩', '女孩'],
-			date: currentDate,
-			districtIndex: 0,
-			district: ['禅城', '南海', '顺德', '高明', '三水'],
+			provinceKey: '',
+			cityKey: '',
+			provinceArray: [],
+			cityArray: [],
+			areaArray: [],
+			region: [
+				[],
+				[],
+				[],
+			],
+			pKeysArray: [],
+			cKeysArray: [],
+			aKeysArray: [],
+			regionResult: [
+				[],
+				[],
+				[],
+			],
+			selectedProvinceCode: '',
+			selectedCityCode: '',
+			selectedAreaCode: '',
+			selectedProvinceValue: '',
+			selectedCityValue: '',
+			selectedAreaValue: '',
 			kindergartenIndex: 0,
 			kindergarten: ['裴蕾幼儿园', '育英幼儿园', '财贸幼儿园', '机关幼儿园'],
 			gradeIndex: 0,
@@ -96,24 +93,60 @@ export default {
         }
     },
 	computed: {
-		startDate() {
-			return this.getDate('start');
-		},
-		endDate() {
-			return this.getDate('end');
+		finalRegionString() {
+			return this.selectedProvinceValue + this.selectedCityValue + this.selectedAreaValue
 		}
 	},
     onLoad() {
-        
+        this.provinceArray = Object.values(regionData['86'])
+		this.pKeysArray = Object.keys(regionData['86'])
+		this.region[0] = this.provinceArray
     },
     methods: {
-		bindSexChange: function(e) {
-			console.log('picker发送选择改变，携带值为', e.target.value)
-			this.sexIndex = e.target.value
+		regionColChange(e) {
+			// console.log(e)
+			let col = e.detail.column
+			let val = e.detail.value
+			// console.log(col, val)
+			
+			if (col === 0) {
+				this.provinceKey = Object.keys(regionData['86'])[val]
+				this.cityArray = Object.values(regionData[this.provinceKey]) 
+				this.region[1] = this.cityArray
+				this.cKeysArray = Object.keys(regionData[this.provinceKey])
+				this.selectedProvinceValue = Object.values(regionData['86'])[val]
+				this.selectedCityValue = ''
+				this.selectedAreaValue = ''
+				// this.regionResult[0] = this.provinceArray[val]
+				// this.regionResult[1] = this.cityArray[0]
+				// this.regionResult[2] = []
+			}
+			if (col === 1 ) {
+				this.cityKey = Object.keys(regionData[this.provinceKey])[val]
+				this.selectedCityValue = Object.values(regionData[this.provinceKey])[val]
+				this.areaArray = Object.values(regionData[this.cityKey])
+				this.region[2] = this.areaArray
+				this.aKeysArray = Object.keys(regionData[this.cityKey])
+				this.selectedAreaValue = ''
+				// this.regionResult[1] = this.cityArray[val]
+				// this.regionResult[2] = this.areaArray[0]
+			}
+			if (col === 2) {
+				this.selectedAreaValue = Object.values(regionData[this.cityKey])[val]
+			}
+			// this.$forceUpdate()
 		},
-		bindDistrictChange: function(e) {
-			console.log('picker发送选择改变，携带值为', e.target.value)
-			this.districtIndex = e.target.value
+		regionChange(e) {
+			// this.selectedProvinceValue = e.target.value[0] ? this.provinceArray[e.target.value[0]] : this.provinceArray[0]
+			// this.selectedCityValue = e.target.value[1] ? this.cityArray[e.target.value[1]] : this.cityArray[0]
+			// this.selectedAreaValue = e.target.value[2] ? this.areaArray[e.target.value[2]] : this.areaArray[0]
+			
+			this.selectedProvinceCode = this.pKeysArray[e.target.value[0]]
+			this.selectedCityCode = this.cKeysArray[e.target.value[1]]
+			this.selectedAreaCode = this.aKeysArray[e.target.value[2]]
+			
+			console.log(e.target.value)
+			console.log(this.selectedProvinceCode, this.selectedCityCode, this.selectedAreaCode)
 		},
 		bindKindergartenChange: function(e) {
 			console.log('picker发送选择改变，携带值为', e.target.value)
@@ -126,24 +159,6 @@ export default {
 		bindTeamChange: function(e) {
 			console.log('picker发送选择改变，携带值为', e.target.value)
 			this.teamIndex = e.target.value
-		},
-		getDate(type) {
-			const date = new Date();
-			let year = date.getFullYear();
-			let month = date.getMonth() + 1;
-			let day = date.getDate();
-
-			if (type === 'start') {
-				year = year - 60;
-			} else if (type === 'end') {
-				year = year + 2;
-			}
-			month = month > 9 ? month : '0' + month;;
-			day = day > 9 ? day : '0' + day;
-			return `${year}-${month}-${day}`;
-		},
-		bindDateChange: function(e) {
-			this.date = e.target.value
 		},
 		goIndex() {
 			uni.reLaunch({ url: '../index/index' })
@@ -217,5 +232,8 @@ export default {
 	background: #fff;
 	margin-top: 30upx;
 	border: 1px solid #f1f1f1;
+}
+.picker-input {
+	width: 60%;
 }
 </style>
