453617b17f7e2aad86ca60590f8e970ded8afc53
Decade <sitting2002@126.com>
2020-05-25 10:47:27 +0800

    mod my

--- main.js
+++ main.js
@@ -10,6 +10,7 @@ import url from './common/config.js'
 Vue.prototype.$request = request
 Vue.prototype.$api = api
 Vue.prototype.$url = url
+Vue.prototype.$aliImage = 'https://et-pic-server.oss-cn-shenzhen.aliyuncs.com/app_img/'
 
 App.mpType = 'app'
 
--- pages/my/my.vue
+++ pages/my/my.vue
@@ -6,22 +6,24 @@
 			<view class="userInfo-position">
 				<view class="user-left-position">
 					<view class="userInfo-content">
-						<text style="color: #FFFFFF;font-size: 30upx;">{{userInfo.babyName}}</text>
+						<text style="color: #FFFFFF;font-size: 30upx;">{{userInfo.name}}</text>
 						<view class="white-space-width"></view>
-						<image src="../../static/my/diamond.png" style="width: 40upx; height: 40upx;"></image>
+						<!-- <image src="../../static/my/diamond.png" style="width: 40upx; height: 40upx;"></image> -->
 					</view>
 					
 					<view style="height: 10upx;"></view>
 					
+					<!-- 
 					<view class="userInfo-member">
 						<view class="userInfo-member-border">
 							<text>会员截止日期：{{userInfo.overTime}}</text>
 						</view>
-					</view>
+					</view> 
+					-->
 				</view>
 				
 				<view class="user-right-position">
-					<image src="../../static/cart/test.png" style="border-radius: 50%; width: 120upx; height: 120upx;"></image>
+					<image :src="userInfo.avatar" style="border-radius: 50%; width: 120upx; height: 120upx;"></image>
 				</view>
 			</view>
 			
@@ -56,62 +58,67 @@
 import etMyBox from '../../components/etMyBox.vue'
 
 export default {
+	computed: {
+		userInfo() {
+			return uni.getStorageSync('userInfo')
+		}
+	},
 	components: {
 		etMyBox
 	},
 	data() {
 		return {
 			test: '123456',
-			userInfo: {
-				'name':'开发者',
-				'overTime':'20120-05-19',
-				'babyName':'开发者baby',
-				'school':'林头幼儿园',
-				'class':'一年一班'
-			},
+			// userInfo: {
+			// 	'name':'开发者',
+			// 	'overTime':'20120-05-19',
+			// 	'babyName':'开发者baby',
+			// 	'school':'林头幼儿园',
+			// 	'class':'一年一班'
+			// },
 			myOrderInfo: {
 				'menuTitle' : '我的订单',
 				'moreMenu' : '更多 >',
 				'allMenu': [
 					{
-						'imgSrc' : '../static/cart/right.png',
+						'imgSrc' : this.$aliImage + 'my1_1.png',
 						'title'	: '待支付',
 						'toUrl'	: '/pages/index/bookdetail?bookID=900',
 						'count'	: '99+'
 					},
 					{
-						'imgSrc' : '../static/cart/right.png',
-						'title'	: '待支付',
+						'imgSrc' : this.$aliImage + 'my1_2.png',
+						'title'	: '待发货',
 						'toUrl'	: 'bookdetail?bookID=901'
 					},
 					{
-						'imgSrc' : '../static/cart/right.png',
-						'title'	: '待支付',
+						'imgSrc' : this.$aliImage + 'my1_3.png',
+						'title'	: '待收货',
 						'toUrl'	: 'bookdetail?bookID=900'
 					},
 					{
-						'imgSrc' : '../static/cart/right.png',
-						'title'	: '待支付',
+						'imgSrc' : this.$aliImage + 'my1_4.png',
+						'title'	: '阅读中',
 						'count'	: '2'
 					},
 					{
-						'imgSrc' : '../static/cart/right.png',
-						'title'	: '待支付',
+						'imgSrc' : this.$aliImage + 'my1_5.png',
+						'title'	: '待还书',
 						'toUrl'	: 'bookdetail?bookID=900'
 					},
 					{
-						'imgSrc' : '../static/cart/right.png',
-						'title'	: '待支付',
+						'imgSrc' : this.$aliImage + 'my1_6.png',
+						'title'	: '待评价',
 						'toUrl'	: 'bookdetail?bookID=900'
 					},
 					{
-						'imgSrc' : '../static/cart/right.png',
-						'title'	: '待支付',
+						'imgSrc' : this.$aliImage + 'my1_7.png',
+						'title'	: '退款',
 						'toUrl'	: 'bookdetail?bookID=900'
 					},
 					{
-						'imgSrc' : '../static/cart/right.png',
-						'title'	: '待支付',
+						'imgSrc' : this.$aliImage + 'my1_8.png',
+						'title'	: '逾期',
 						'toUrl'	: 'bookdetail?bookID=900'
 					}
 				]
@@ -120,46 +127,46 @@ export default {
 				'menuTitle' : '我的应用',
 				'allMenu': [
 					{
-						'imgSrc' : '../static/cart/right.png',
-						'title'	: '待支付',
+						'imgSrc' : this.$aliImage + 'my2_1.png',
+						'title'	: '押金',
 						'toUrl'	: 'bookdetail?bookID=900',
 						'buttomContent' : '$ 200'
 					},
 					{
-						'imgSrc' : '../static/cart/right.png',
-						'title'	: '待支付',
+						'imgSrc' : this.$aliImage + 'my2_2.png',
+						'title'	: '积分',
 						'toUrl'	: 'bookdetail?bookID=900',
 						'buttomContent' : '2000'
 					},
 					{
-						'imgSrc' : '../static/cart/right.png',
-						'title'	: '待支付',
+						'imgSrc' : this.$aliImage + 'my2_3.png',
+						'title'	: '签到',
 						'toUrl'	: 'bookdetail?bookID=900',
 						'buttomContent' : '0/1'
 					},
 					{
-						'imgSrc' : '../static/cart/right.png',
-						'title'	: '待支付',
+						'imgSrc' : this.$aliImage + 'my2_4.png',
+						'title'	: '活动',
 						'count'	: '2'
 					},
 					{
-						'imgSrc' : '../static/cart/right.png',
-						'title'	: '待支付',
+						'imgSrc' : this.$aliImage + 'my2_5.png',
+						'title'	: '完善信息',
 						'toUrl'	: 'bookdetail?bookID=900'
 					},
 					{
-						'imgSrc' : '../static/cart/right.png',
-						'title'	: '待支付',
+						'imgSrc' : this.$aliImage + 'my2_6.png',
+						'title'	: '会员',
 						'toUrl'	: 'bookdetail?bookID=900'
 					},
 					{
-						'imgSrc' : '../static/cart/right.png',
-						'title'	: '待支付',
+						'imgSrc' : this.$aliImage + 'my2_7.png',
+						'title'	: '反馈',
 						'toUrl'	: 'bookdetail?bookID=900'
 					},
 					{
-						'imgSrc' : '../static/cart/right.png',
-						'title'	: '待支付',
+						'imgSrc' : this.$aliImage + 'my2_8.png',
+						'title'	: '收获地址',
 						'toUrl'	: 'bookdetail?bookID=900'
 					}
 				]
