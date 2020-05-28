520d31b0e3ef237711f887767e6950f599a5d525
Decade <sitting2002@126.com>
2020-05-28 16:54:00 +0800

    add date

--- common/formatDate.js
+++ common/formatDate.js
@@ -1,15 +1,36 @@
 // 日期加减 并且格式化
 // new Date('2019/09/29 10:56')
 // n
-function formatDate(today, n) {
+function getDateDuration(today, n) {
 　　//加上N天的时间
     let s1 = today.getTime() + 3600 * 1000 * 24 * n
-    fillTime = moment(s1).format('YYYY-MM-DD HH:mm:ss')
+    fillTime = formatDate(new Date(s1))
     return fillTime
+}
+
+function formatDate(date) {
+    let seperator1 = '-';
+    let year = date.getFullYear();
+    let month = date.getMonth() + 1;
+    let day = date.getDate();
+    if (month >= 1 && month <= 9) {
+        month = '0' + month;
+    }
+    if (day >= 0 && day <= 9) {
+        day = '0' + day;
+    }
+	
+	let hour = date.getHours() 
+	let minute = date.getMinutes()
+	let second = date.getSeconds()
+	
+    let currentdate = year + seperator1 + month + seperator1 + day hour + ':' + minute
+    return currentdate;
 }
 
 
 module.exports = {
-    formatDate
+    formatDate,
+	getDateDuration
 };
 
