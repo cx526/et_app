322bd76b66993b4a5b026a4658aadfc610f3270a
hojohnfit <your user email>
2020-06-08 15:34:30 +0800

    add pages

--- pages/index/kindlist.vue
+++ pages/index/kindlist.vue
@@ -1,24 +1,51 @@
 <template>
 	<view class="content">
-		<view class="tabs-content">
-			<cl-tabs class="tabs" :tab-bars="tabBars" :tab-index="tabCurrentIndex" @tabChange="tabChange" :sliderMargin="15"
-					:scale="1.3" sliderColor="#faff72" type="float" ref="tabs0" aniType="extend">
-			</cl-tabs>
-		</view>
-		
-		
-		<view class="list-content-father-position">
-			<view class="list-content-father-position" v-if="listData.length > 0">
-				<view class="list-content-position">					
-					<et-imgbox  v-for="(item,i) in listData" :key="i" :bookInfo="item.goods_info"></et-imgbox>
+		<view class="content-position">
+			<!-- 顶部导航栏 -->
+			<view class="tabs-content">
+				<cl-tabs class="tabs" :tab-bars="tabBars" :tab-index="tabCurrentIndex" @tabChange="tabChange" :sliderMargin="15"
+						:scale="1.3" sliderColor="#faff72" type="float" ref="tabs0" aniType="extend">
+				</cl-tabs>
+			</view>
+			
+			<!-- 数据列表 -->
+			<view class="list-content-father-position">
+				<view class="list-content-father-position" v-if="listData.length > 0">
+					<view class="list-content-position">					
+						<et-imgbox  v-for="(item,i) in listData" :key="i" :bookInfo="item.goods_info" @insertBookToCart="insertBookToCart"></et-imgbox>
+					</view>
+					<view class="white-space"></view>
+					<uni-load-more :status="loadStatus" :content-text="loadText" />
 				</view>
-				<view class="white-space"></view>
-				<uni-load-more :status="loadStatus" :content-text="loadText" />
-			</view>
-			<view class="empty-style" v-else>
-				<text>列表空空如也</text>
+				<view class="empty-style" v-else>
+					<text>列表空空如也</text>
+				</view>
 			</view>
 		</view>
+		
+		<!-- 书篮浮窗 -->
+		<view class="book-cart-position" @tap="openBookCartList">
+			<view class="book-cart-content-position">
+				<view class="book-cart-style">
+					<image src="https://et-pic-server.oss-cn-shenzhen.aliyuncs.com/app_img/bookdetail_cart.png" class="book-cart-img-style"></image>
+				</view>
+				<view class="book-cart-count-style">
+					<text>{{cartBookCount}}</text>
+				</view>
+			</view>
+		</view>
+		
+		<!-- 书篮弹窗 -->
+		<uni-popup ref="popup" type="top">
+			<!-- <view style="overflow-y: scroll;">
+				<et-book-cart-list></et-book-cart-list>
+			</view> -->
+			<scroll-view scroll-y="true" >
+				<view>
+					<et-book-cart-list></et-book-cart-list>
+				</view>
+			</scroll-view>
+		</uni-popup>
 		
 	</view>
 </template>
@@ -27,12 +54,22 @@
 import clTabs from '../../components/cl-tabs/cl-tabs.vue'
 import etKindlist from '../../components/etKindlist.vue'
 import etImgbox from '../../components/etImgbox.vue'
+import etBookCartList from '../../components/etBookCartList.vue'
+import uniPopup from '@/components/uni-popup/uni-popup.vue'
+import uniPopupMessage from '@/components/uni-popup/uni-popup-message.vue'
+import uniPopupDialog from '@/components/uni-popup/uni-popup-dialog.vue'
+
+const insertBook = require('@/common/carDataOption');
 
 export default {
 	components: {
 		clTabs,
 		etKindlist,
-		etImgbox
+		etImgbox,
+		etBookCartList,
+		uniPopup,
+		uniPopupMessage,
+		uniPopupDialog
 	},
 	data() {
 		return {
@@ -53,9 +90,13 @@ export default {
 				contentrefresh: '加载中',
 				contentnomore: '没有更多'
 			},
+			cartBookCount: "",
 		}
 	},
 	onLoad(option) {
+		//初始化书篮书本数量
+		this.cartBookCount = insertBook.cartBookCount();
+		
 		//初始化是否允许上拉更新数据,有些页面复用不能更新数据
 		if(option.noPull){
 			this.noPull = parseInt(option.noPull);
@@ -184,13 +225,22 @@ export default {
 		},
 		toDetail(id){
 			uni.navigateTo({url: 'bookdetail?bookID=' + JSON.stringify(id)})
+		},
+		insertBookToCart(){
+			this.cartBookCount = insertBook.cartBookCount();
+		},
+		openBookCartList(){
+			this.$refs.popup.open();
 		}
 	}
 }
 </script>
 
-<style>
+<style scoped>
 .content {
+	display: flex;
+}
+.content-position {
 	display: flex;
 }
 .tabs-content {
@@ -225,5 +275,47 @@ export default {
 .list-content {
 	width: 45%;
 	margin-right: 4%;
+}
+.book-cart-position {
+	position: fixed;
+	right: 0;
+	bottom: 400upx;
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+}
+.book-cart-content-position {
+	width: 90upx;
+	height: 90upx;
+	border-radius: 50%;
+	padding: 10upx;
+	background-color: rgba(196,198,206,.7);
+	position: relative;
+	
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+}
+.book-cart-img-style {
+	width: 70upx;
+	height: 70upx;
+}
+.book-cart-count-style {
+	position: absolute;
+	left: 60upx;
+	top: 10upx;
+	width: 40upx;
+	height: 40upx;
+	border-radius: 50%;
+	color: #FFFFFF;
+	font-size: 20upx;
+	background-color: #FA5151;
+	
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
 }
 </style>
