0f94d33bd28cbe3256e8d63dc944ab69b32936e4
lichenli <1017530553@qq.com>
2020-08-11 18:07:00 +0800

    my page fix css style

--- pages/my/my.vue
+++ pages/my/my.vue
@@ -1,25 +1,16 @@
 <template>
 	<view class="content">
-		<view class="top-position"></view>
-		
-		<view class="content-position">
+		<!-- 顶部底图 -->
+		<view class="top-position">
 			<view class="userInfo-position">
 				<view class="user-left-position">
 					<view class="userInfo-content">
 						<text style="color: #FFFFFF;font-size: 40upx;">{{userInfo.name === 'guest' ? '五车书游客, 您好！' : userInfo.name}}</text>
 						<view class="white-space-width"></view>
-						<!-- <image src="../../static/my/diamond.png" style="width: 40upx; height: 40upx;"></image> -->
 					</view>
 					
 					<view style="height: 10upx;"></view>
 					
-					<!-- 
-					<view class="userInfo-member">
-						<view class="userInfo-member-border">
-							<text>会员截止日期：{{userInfo.overTime}}</text>
-						</view>
-					</view> 
-					-->
 				</view>
 				
 				<view class="user-right-position">
@@ -31,24 +22,106 @@
 					</view>
 				</view>
 			</view>
-			
-			<!-- <view class="vip-button-position"  @tap="vipButton">
-				<image style="width: 640upx; height: 145upx;" src="https://et-pic-server.oss-cn-shenzhen.aliyuncs.com/app_img/vip_button.png"></image>
-			</view> -->
-			
-			<!-- <view class="white-space"></view> -->
-			
-			<!-- <view class="vip-button-position" @tap="vipBanner">
-				<image style="width: 660upx; height: 200upx;" src="https://et-pic-server.oss-cn-shenzhen.aliyuncs.com/app_img/vip_banner.png"></image>
+		</view>
+		
+		<view class="content-position">
+			<!-- <view class="userInfo-position">
+				<view class="user-left-position">
+					<view class="userInfo-content">
+						<text style="color: #FFFFFF;font-size: 40upx;">{{userInfo.name === 'guest' ? '五车书游客, 您好！' : userInfo.name}}</text>
+						<view class="white-space-width"></view>
+					</view>
+					
+					<view style="height: 10upx;"></view>
+					
+				</view>
+				
+				<view class="user-right-position">
+					<view v-if="userInfo.name === 'guest'" @tap="goAuth">
+						<image src="https://et-pic-server.oss-cn-shenzhen.aliyuncs.com/app_img/avatar.png" style="border-radius: 50%; width: 120upx; height: 120upx;"></image>
+					</view>
+					<view v-else @tap="clearSessionAction">
+						<image :src="userInfo.avatar" style="border-radius: 50%; width: 120upx; height: 120upx;"></image>
+					</view>
+				</view>
 			</view> -->
 			
 			<view class="white-space"></view>
-			
-			<view class="my-box-position" v-if="updateOrderInfo">
-				<etMyBox :myMenuInfo="myOrderInfo"></etMyBox>
+			<!-- 线下订单 -->
+			<view class="offline-box">
+				<view class="offline">
+					<view class="offline-order" 
+					style="border-radius: 0;
+					box-shadow: none;
+					border-top-left-radius: 20rpx;
+					border-top-right-radius: 20rpx;">
+						<view class="left">
+							<image src="https://et-pic-server.oss-cn-shenzhen.aliyuncs.com/app_img/offline-icon-all.png" mode=""></image>
+							<text>智慧书柜订单</text>
+						</view>
+						<view class="center">
+							<image src="https://et-pic-server.oss-cn-shenzhen.aliyuncs.com/app_img/offline-cut-line.png" mode="widthFix"></image>
+						</view>
+						<view class="right">
+							<block 
+							v-for="(item, index) in offlineOrderList" 
+							:key="index">
+								<view class="item" @tap="goOffline(item.url)">
+									<image :src="item.imgUrl" mode="widthFix"></image>
+									<text>{{ item.text }}</text>
+								</view>
+							</block>
+						</view>
+					</view>
+					<view
+					style="width: 100%; box-sizing: border-box; padding: 0 18rpx;">
+						<uni-notice-bar
+						scrollable="true" 
+						single="true" 
+						text="您有一笔订单将逾期,请及时处理!" 
+						backgroundColor="#EBF8FF"
+						color="#333" 
+						showIcon
+						:single="true">
+						</uni-notice-bar>
+					</view>
+				</view>
+				
+				
+				
 			</view>
 			
-			<view class="white-space" style="height: 25upx;"></view>
+			
+			<!-- 线上订单 -->
+			<view class="offline-box">
+				<view class="offline-order">
+					<view class="left" @tap="goOnlineAll">
+						<image src="https://et-pic-server.oss-cn-shenzhen.aliyuncs.com/app_img/online-icon-00.png" mode=""></image>
+						<text>邮寄借阅订单</text>
+					</view>
+					<view class="center">
+						<image src="https://et-pic-server.oss-cn-shenzhen.aliyuncs.com/app_img/offline-cut-line.png" mode="widthFix"></image>
+					</view>
+					<view class="online-right">
+						<scroll-view scroll-x>
+							<block 
+							v-for="(item, index) in onlineOrderList" 
+							:key="index">
+								<view class="item" @tap="goOnline(item.url)">
+									<image :src="item.imgUrl" mode=""></image>
+									<view>{{ item.text }}</view>
+								</view>
+							</block>
+						</scroll-view>
+						
+					</view>
+					<image
+					src="https://et-pic-server.oss-cn-shenzhen.aliyuncs.com/app_img/offline-icon-right.png"
+					style="position: absolute;right: 12rpx;top: 50%;transform: translateY(-50%);width: 24rpx;height: 24rpx;"></image>
+				</view>
+				
+				
+			</view>
 			
 			<view class="my-box-position">
 				<etMyBox :myMenuInfo="myAppInfo"></etMyBox>
@@ -61,6 +134,10 @@
 			</view>
 			
 			<view class="white-space"></view>
+			<!-- 底图 -->
+			<view class="bottom-banner" @tap="phone">
+				<image src="https://et-pic-server.oss-cn-shenzhen.aliyuncs.com/app_img/my-bottom-banner.png" mode="widthFix"></image>
+			</view>
 		</view>
 	</view>
 </template>
@@ -68,6 +145,7 @@
 <script>
 import etMyBox from '../../components/etMyBox.vue'
 import etMyReadBookData from '../../components/etMyReadBookData.vue'
+import uniNoticeBar from '@/components/uni-notice-bar/uni-notice-bar.vue'
 const toUrlFunction = require('@/common/toUrlFunction');
 const checkLogin = require('@/common/checkLogin');
 const bookListData = require('@/common/carDataOption');
@@ -80,19 +158,13 @@ export default {
 	},
 	components: {
 		etMyBox,
-		etMyReadBookData
+		etMyReadBookData,
+		uniNoticeBar
 	},
 	data() {
 		return {
 			updateOrderInfo: false,
 			test: '123456',
-			// userInfo: {
-			// 	'name':'开发者',
-			// 	'overTime':'20120-05-19',
-			// 	'babyName':'开发者baby',
-			// 	'school':'林头幼儿园',
-			// 	'class':'一年一班'
-			// },
 			myOrderInfo: {
 				'menuTitle' : '我的订单',
 				'moreMenu' : '更多 >',
@@ -213,7 +285,72 @@ export default {
 						'count'	: '0'
 					}
 				]
-			}
+			},
+			offlineOrderList: [
+				{
+					imgUrl: 'https://et-pic-server.oss-cn-shenzhen.aliyuncs.com/app_img/offline-icon-01.png',
+					text: '待取书',
+					url: '/pages/library/take-books?status=0'
+				},
+				{
+					imgUrl: 'https://et-pic-server.oss-cn-shenzhen.aliyuncs.com/app_img/offline-icon-02.png',
+					text: '待还书',
+					url: '/pages/library/return-books?status=0'
+				},
+				{
+					imgUrl: 'https://et-pic-server.oss-cn-shenzhen.aliyuncs.com/app_img/offline-icon-03.png',
+					text: '已归还',
+					url: '/pages/library/return-books?status=1'
+				},
+				{
+					imgUrl: 'https://et-pic-server.oss-cn-shenzhen.aliyuncs.com/app_img/offline-icon-04.png',
+					text: '已失效',
+					url: '/pages/library/take-books?status=1'
+				},
+			],
+			onlineOrderList: [
+				{
+					imgUrl: 'https://et-pic-server.oss-cn-shenzhen.aliyuncs.com/app_img/online-icon-01.png',
+					text: '待支付',
+					url: '/pages/cart/orderList?status_text=待支付'
+				},
+				{
+					imgUrl: 'https://et-pic-server.oss-cn-shenzhen.aliyuncs.com/app_img/online-icon-02.png',
+					text: '待发货',
+					url: '/pages/cart/orderList?status_text=待发货'
+				},
+				{
+					imgUrl: 'https://et-pic-server.oss-cn-shenzhen.aliyuncs.com/app_img/online-icon-03.png',
+					text: '待收货',
+					url: '/pages/cart/orderList?status_text=待收货'
+				},
+				{
+					imgUrl: 'https://et-pic-server.oss-cn-shenzhen.aliyuncs.com/app_img/online-icon-04.png',
+					text: '待归还',
+					url: '/pages/cart/orderList?status_text=待还书'
+				},
+				{
+					imgUrl: 'https://et-pic-server.oss-cn-shenzhen.aliyuncs.com/app_img/online-icon-05.png',
+					text: '待取件',
+					url: '/pages/cart/orderList?status_text=待取件'
+				},
+				{
+					imgUrl: 'https://et-pic-server.oss-cn-shenzhen.aliyuncs.com/app_img/online-icon-06.png',
+					text: '待评价',
+					url: '/pages/cart/orderList?status_text=待评价'
+				},
+				{
+					imgUrl: 'https://et-pic-server.oss-cn-shenzhen.aliyuncs.com/app_img/online-icon-07.png',
+					text: '退款',
+					url: '/pages/cart/orderList?status_text=退款'
+				},
+				{
+					imgUrl: 'https://et-pic-server.oss-cn-shenzhen.aliyuncs.com/app_img/online-icon-08.png',
+					text: '逾期',
+					url: '/pages/cart/orderList?status_text=逾期'
+				},
+			],
+			
 		}
 	},
 	onLoad() {
@@ -261,6 +398,7 @@ export default {
 		},
 		getOrderCount(){
 			let guestStatus = checkLogin.checkLogin(true);
+			console.log(guestStatus)
 			if(guestStatus){
 				this.updateOrderInfo = true
 				return;
@@ -296,12 +434,142 @@ export default {
 					})
 				});
 			});
-		}
-	}
+		},
+		// 线下订单页跳转
+		goOffline(url) {
+			uni.navigateTo({
+				url
+			})
+		},
+		// 线上订单页跳转
+		goOnline(url) {
+			uni.navigateTo({
+				url
+			})
+		},
+		// 查看线上全部订单
+		goOnlineAll() {
+			uni.navigateTo({
+				url: '/pages/cart/orderList?status_text=全部'
+			})
+		},
+		// 拨打号码
+		phone() {
+			uni.makePhoneCall({
+			    phoneNumber: '400-0022-708' //仅为示例
+			});
+		},
+	},
+		
 }
 </script>
 
 <style scoped>
+/* 线下订单 */
+.offline-box {
+	width: 100%;
+	box-sizing: border-box;
+	margin-bottom: 24rpx;
+	padding: 0 24rpx;
+}
+.offline-box .offline {
+	background-color: rgb(255,255,255) !important;
+	box-sizing: border-box;
+	padding-bottom: 10rpx;
+	box-shadow: 0rpx 0rpx 20rpx rgba(179, 179, 179, 0.3) ;
+	border-radius: 20rpx;
+}
+.offline-box .offline-order {
+	display: flex;
+	align-items: center;
+	box-sizing: border-box;
+	padding: 36rpx 0;
+	background: #fff;
+	border-radius: 20rpx;
+	box-shadow: 0rpx 0rpx 20rpx rgba(179, 179, 179, 0.3);
+	font-size: 24rpx;
+	position: relative;
+}
+.offline-box .offline-order .left {
+	width: 180rpx;
+	flex-shrink: 0;
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+	font-size: 24rpx;
+	font-weight: bold;
+	position: relative;
+	left: 9rpx;
+
+}
+.offline-box .offline-order .center {
+	flex-shrink: 0;
+}
+.offline-box .offline-order image {
+	width: 26rpx;
+	height: 110rpx;
+}
+.offline-box .offline-order .right {
+	width: 496rpx;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	box-sizing: border-box;
+	padding: 0 24rpx 0 18rpx;
+}
+.offline-box .offline-order .right .item {
+	flex: 1;
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+	color: #666;
+
+}
+.offline-box .offline-order .right .item image {
+	width: 60rpx;
+	height: 60rpx;
+}
+.offline-box .offline-order .online-right {
+	flex: 1;
+}
+.offline-box .offline-order .online-right scroll-view {
+	white-space: nowrap;
+	width: 496rpx;
+	box-sizing: border-box;
+	padding: 0 24rpx 0 18rpx;
+	
+}
+.offline-box .offline-order .online-right .item {
+	display: inline-block;
+	width: 25%;
+	text-align: center;
+	font-size: 24rpx;
+	color: #666;
+	
+}
+.offline-box .offline-order .online-right .item image {
+	width: 60rpx;
+	height: 60rpx;
+	display: block;
+	position: relative;
+	left: 50%;
+	transform: translateX(-30rpx);
+}
+.offline-box .offline-order .left image {
+	width: 60rpx;
+	height: 60rpx;
+}
+/* 底图 */
+.bottom-banner {
+	box-sizing: border-box;
+	width: 100%;
+	padding-bottom: 24rpx;
+}
+.bottom-banner image {
+	width: 100%;
+}
 .content {
 	display: flex;
 	flex-direction: column;
@@ -318,13 +586,12 @@ export default {
 	width: 100%;
 }
 .content-position {
-	position: absolute;
-	top: 0;
 	z-index: 99;
 	display: flex;
 	flex-direction: column;
 	justify-content: center;
 	align-items: center;
+	margin-top: -150rpx;
 }
 .userInfo-position {
 	width: 80%;
@@ -333,6 +600,7 @@ export default {
 	justify-content: space-between;
 	align-items: center;
 	padding: 40upx 0;
+	margin: 0 auto;
 }
 .user-left-position {
 	display: flex;
@@ -347,7 +615,7 @@ export default {
 	align-items: center;
 }
 .userInfo-member-border {
-	background-color: #2AAEC4;
+	/* background-color: #2AAEC4; */
 	border-radius: 30upx;
 	color: #FFFFFF;
 	font-size: 20UPX;
