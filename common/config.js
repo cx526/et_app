944c5845c92d403f5d9272a2811d3140c5b4d756
Decade <sitting2002@126.com>
2020-09-11 11:39:10 +0800

    fix url

--- common/config.js
+++ common/config.js
@@ -3,10 +3,10 @@ let url_config = ""
 if(process.env.NODE_ENV === 'development'){
     // 开发环境
     url_config = 'http://47.112.145.215:7001'
-		// url_config = 'https://www.52diyike.com/api'
+		// url_config = 'https://52diyike.com/api'
 }else{
     // 生产环境
-    url_config = 'https://www.52diyike.com/api'
+    url_config = 'https://52diyike.com/api'
 		// url_config = 'http://47.112.145.215:7001'
 }
 
