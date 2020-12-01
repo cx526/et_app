252864ad73bf5fc2dd13b3c96e1aab98012005d9
Decade <sitting2002@126.com>
2020-12-01 17:37:07 +0800

    add xm 3

--- pagesFavourite/XMradio/index.vue
+++ pagesFavourite/XMradio/index.vue
@@ -10,7 +10,8 @@
 	export default {
 		data() {
 			return {
-				
+				XMclient: null,
+				XMplayer: null,
 			}
 		},
 		components: {
@@ -22,13 +23,17 @@
 		methods: {
 			init() {
 				// 初始化
-				XMPlugin.init({
-					appKey: 'b617866c20482d133d5de66fceb37da3', // 必传，官方提供的 appKey：b617866c20482d133d5de66fceb37da3 仅供测试，有调用次数限制，上线需替换为自己的 appKey。
+				const { xmly, player } = XMPlugin.init({
+					appKey: 'e604afd5371b12fdfb5428992c3d26a2', // 必传，官方提供的 appKey：b617866c20482d133d5de66fceb37da3 仅供测试，有调用次数限制，上线需替换为自己的 appKey。
 					// 官方提供测试使用的 sigUrl => https://api.ximalaya.com/openapi-collector-app/jssdk_sig
 					// （限制了 appKey 必须是测试账号，并且限制了调用次数，上线需替换为自己的 sigUrl）
-					sigUrl: 'https://api.ximalaya.com/openapi-collector-app/jssdk_sig', // 免登录授权，必须传 sigUrl，和 accessTokenUrl、getAccessToken 互斥
+					// 免登录授权，必须传 sigUrl，和 accessTokenUrl、getAccessToken 互斥
+					// sigUrl: 'https://api.ximalaya.com/openapi-collector-app/jssdk_sig', 
+					sigUrl: 'http://127.0.0.1:7001/api/xmly/signature',
 					debug: true, // 是否在控制台打印日志
 				})
+				this.XMclient = xmly
+				this.XMplayer = player
 			}
 		}
 	}
