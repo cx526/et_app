f4ce2f6a848cebc220c59a6a7b2f1d2e7d3f9b7b
Decade <sitting2002@126.com>
2020-12-07 16:08:33 +0800

    add XM 6

--- pagesFavourite/XMradio/index.vue
+++ pagesFavourite/XMradio/index.vue
@@ -2,6 +2,15 @@
 	<view>
 		<text>XMradio</text>
 		<button @tap="play">play</button>
+		<button @tap="pause">pause</button>
+		<button @tap="stop">stop</button>
+		
+		<view>
+			<view v-for="(item, i) in albumsList" :key="i" style="height: 200upx; background: #00B7CC; color: #fff; margin-top: 30upx;" @tap="goAlbumDetail(item.id)">
+				<image :src="item.cover_url_middle" style="height: 180upx; width: 180upx;"/>
+				<text>{{item.album_title}}</text>
+			</view>
+		</view>
 	</view>
 </template>
 
@@ -22,6 +31,8 @@
 				XMplayer: null,
 				albumsPage: 1,
 				columnPage: 1,
+				albumsList: [],
+				kindList: [],
 			}
 		},
 		components: {
@@ -36,51 +47,79 @@
 				this.XMclient = xmly
 				this.XMplayer = player
 				
-				// 获取开发者收藏专辑
-				let paramAlbums = {
-					page: this.albumsPage
-				}
-				const albumsResult = await this.XMclient.get(XMdeveloper_albumsURL, paramAlbums)
-				if(albumsResult.code === 0) {
-					console.log(albumsResult)
-				}
+				this.albumsList = await this.getAlbumsList(this.albumsPage)
+				console.log(this.albumsList)
 				
-				// 获取开发者听单
-				let paramCol = {
-					page: this.columnPage
-				}
-				const columnResult = await this.XMclient.get(XMdeveloper_columnsURL, paramCol)
-				if(columnResult.code === 0) {
-					console.log(columnResult)
-				}
-				
+				this.kindList = await this.getKinds()
+				console.log(this.kindList)
+			},
+			async getKinds() {
+				// 获取开发者分类列表
+				const categoriesResult = await this.XMclient.get(XMdeveloper_categoriesURL)
+					.then(res => {
+						if(res.code === 0) {
+							return res.data.operation_categories
+						}
+					})
+				let finalCategor = []
+				categoriesResult.map(item => {
+					if (item.source === 2) {
+						finalCategor.push(item)
+					}
+				})
+				return finalCategor
+			},
+			async getBanners() {
 				// 获取开发者焦点图
 				const bannersResult = await this.XMclient.get(XMdeveloper_bannersURL)
 				if(bannersResult.code === 0) {
-					console.log(bannersResult)
+					// console.log(bannersResult)
+					return bannersResult
 				}
-				
-				// 获取开发者分类列表
-				const categoriesResult = await this.XMclient.get(XMdeveloper_categoriesURL)
-				if(categoriesResult.code === 0) {
-					console.log(categoriesResult)
+				return null
+			},
+			async getAlbumsList(page) {
+				// 获取开发者收藏专辑
+				let paramAlbums = {
+					page: page
 				}
-
+				const albumsResult = await this.XMclient.get(XMdeveloper_albumsURL, paramAlbums)
+					.then(res => {
+						if (res.code === 0) {
+							return res.data.albums
+						}
+						return []
+					})
+				return albumsResult
+			},
+			async getAlbumsDetail(album_id, page) {
 				// 获取专辑详情信息
 				let paramAlbumDetail = {
-					album_id: 2811252,
-					page: 1,
+					album_id: album_id,
+					page: page,
 				}
 				const albumsDetailResult = await this.XMclient.get(XMalbums_browseURL, paramAlbumDetail)
-				if(albumsDetailResult.code === 0) {
-					console.log(albumsDetailResult)
-				}				
-				
+				if (albumsDetailResult.code === 0) {
+					// console.log(albumsDetailResult)
+					return albumsDetailResult
+				}
+				return null
 			},
-			async play() {
+			play() {
 				console.log('play')
-				await this.XMplayer.setPlaylist([28780230])
-				await this.XMplayer.playByIndex(0)
+				this.XMplayer.setPlaylist([28780230])
+				this.XMplayer.playByIndex(0)
+			},
+			pause() {
+				console.log('pause')
+				this.XMplayer.pause()
+			},
+			stop() {
+				console.log('stop')
+				this.XMplayer.stop()
+			},
+			async goAlbumDetail(id) {
+				await this.getAlbumsDetail(id, 1)
 			}
 		}
 	}
