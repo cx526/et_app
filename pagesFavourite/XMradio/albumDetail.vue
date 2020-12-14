ffd9db761ebc38e878bb93e9dd1a2303e5ec6a03
lichenli <1017530553@qq.com>
2020-12-14 18:01:25 +0800

    add some page

--- pagesFavourite/XMradio/albumDetail.vue
+++ pagesFavourite/XMradio/albumDetail.vue
@@ -1,8 +1,394 @@
 <template>
+	<view>
+		<!-- 专辑详情start -->
+		<view class="description">
+			<view class="left">
+				<image :src="cover_url_middle" class="cover"></image>
+				<image :src="$aliImage + 'xmly-play-bg.png'" class="bg"></image>
+			</view>
+			<view class="right">
+				<view class="title">宝宝巴士·睡前故事 | 4-7岁儿童</view>
+				<block v-if="tagList && tagList.length > 0">
+					<view class="tag" >
+						<view v-for="(item, index) in tagList" :key="index" v-if="index <= 2">{{ item }}</view>
+					</view>
+				</block>
+				<view class="play">
+					<image :src="$aliImage + 'xmly-icon-01.png'" mode=""></image>
+					<text>{{ play_count | formatPlayCount }}</text>
+				</view>
+			</view>
+		</view>
+		<!-- 专辑详情end -->
+		<!-- 简介/节目start -->
+		<view class="list">
+			<view class="nav">
+				<view v-for="(item, index) in navList" :key="index" class="item" @tap="changeTab(index)" :class="index === currentIndex ? ' active' : ''">
+					<text >{{ item.title }}</text>
+					<view class="line" :class="index === currentIndex ? ' active' : ''"></view>
+				</view>
+			</view>
+			<view class="options">
+				<view class="item">
+					<image :src="$aliImage + 'xmly-play.png'"></image>
+					<text>全部播放</text>
+				</view>
+				<view class="right">
+					<view class="item">
+						<image :src="$aliImage + 'xmly-sort.png'"></image>
+						<text>排序</text>
+					</view>
+					<view class="item">
+						<image :src="$aliImage + 'xmly-play.png'"></image>
+						<text>选集</text>
+					</view>
+				</view>
+				
+				
+			</view>
+			<view class="context">
+				<swiper :style="{ 'height': swiperHeight }" :current="currentIndex" @change="swiperChange">
+					<swiper-item>
+						<view class="introduction">{{ album_intro }}</view>
+					</swiper-item>
+					<swiper-item>
+						<block v-for="(item, index) in dataList" :key="index">
+							<view class="item">
+								<view class="detail">
+									<view class="sort">{{ index + 1 }}</view>
+									<view class="title">{{ item.track_title }}</view>
+									<view class="icon">
+										<image :src="$aliImage + 'xmly-play.png'"></image>
+									</view>
+								</view>
+								<view class="time">
+									<view class="listen">
+										<image :src="$aliImage + 'xmly-listen.png'"></image>
+										<text>{{ item.play_count | formatPlayCount }}</text>
+									</view>
+									<view class="listen">
+										<image :src="$aliImage + 'xmly-clock.png'"></image>
+										<text>{{ item.created_at }}</text>
+									</view>
+								</view>
+							</view>
+						</block>
+					</swiper-item>
+				</swiper>
+			</view>
+		</view>
+		<!-- 简介/节目end -->
+	</view>
 </template>
 
 <script>
+	import { initXMLY } from './XM.js'
+	import { XMalbums_browseURL } from './XM.js'
+	export default {
+		data() {
+			return {
+				$aliImage: this.$aliImage,
+				album_id: '', // 专辑id
+				tagList: [], // 专辑标签
+				cover_url_middle: '', // 封面
+				play_count: '', // 播放次数
+				album_intro: '', // 专辑简介
+				dataList: [],
+				itemHeight: 0,
+				swiperHeight: 0,
+				XMclient: '',
+				XMplayer: '',
+				currentIndex: 0,
+				page: 1,
+				navList: [
+					{title: '简介'}, 
+					{title: '节目'},
+				]
+			}
+		},
+		async onLoad(options) {
+			let album_detail = JSON.parse(uni.getStorageSync('album_detail'))
+			console.log(album_detail)
+			this.album_id = Number(album_detail.id)
+			this.tagList = album_detail.tagList
+			this.cover_url_middle = album_detail.cover_url_middle
+			this.play_count = album_detail.play_count
+			this.album_intro = album_detail.album_intro
+			await this.init()
+			await this.getAlbumsDetail(this.album_id, this.page)
+		},
+		filters: {
+			formatPlayCount(play_count) {
+				let len = String(play_count).length
+				if(len >= 5 && len < 9) {
+					return play_count = (play_count / 10000).toFixed(2) + '万'
+				}
+				else if(len >= 9) {
+					return play_count = (play_count / 100000000).toFixed(2) + '亿'
+				}
+			}
+		},
+		methods: {
+			// 初始化
+			async init() {
+				const { xmly, player } = await initXMLY()
+				this.XMclient = xmly
+				this.XMplayer = player
+			},
+			async getAlbumsDetail(album_id, page) {
+				// 获取专辑详情信息
+				let paramAlbumDetail = {
+					album_id: album_id,
+					page: page,
+				}
+				await this.XMclient.get(XMalbums_browseURL, paramAlbumDetail).then(res => {
+					console.log(res)
+					if(res.code === 0) {
+						this.dataList = [...this.dataList, ...res.data.tracks]
+						this.getSwiperHeight('.context .item')
+					}
+				})	
+			},
+			// 动态设置swiper高度
+			getSwiperHeight(ele) {
+				setTimeout(() => {
+					const query = uni.createSelectorQuery().in(this);
+					query.select(ele).boundingClientRect(data => {
+					  console.log(data)
+						this.itemHeight = data.height
+						console.log(this.itemHeight)
+						this.swiperHeight = this.dataList.length * this.itemHeight + 'px'
+						console.log(this.swiperHeight)
+					}).exec();
+				}, 200)
+				
+			},
+			// 点击tab
+			changeTab(index) {
+				this.currentIndex = index
+			},
+			// 监听swiper改变
+			swiperChange(event) {
+				let index = event.detail.current
+				this.currentIndex = index
+			},
+		},
+	}
 </script>
 
 <style>
+	page {
+		box-sizing: border-box;
+		background: #EBF8FF;
+		padding: 25rpx;
+	}
+</style>
+<style scoped>
+	/* 专辑详情start */
+	.description {
+		box-sizing: border-box;
+		background: #ffffff;
+		border-radius: 30rpx;
+		box-shadow: 0px 0px 6px 0px rgba(0,0,0,0.16); 
+		padding: 40rpx;
+		display: flex;
+		align-items: center;
+		
+	}
+	.description .left {
+		flex-shrink: 0;
+		position: relative;
+		width: 226rpx;
+		height: 200rpx;
+		position: relative;
+		margin-right: 40rpx;
+	}
+	.description .left .cover {
+		width: 200rpx;
+		height: 200rpx;
+		position: absolute;
+		left: 0;
+		top: 0;
+		z-index: 8;
+	}
+	.description .left .bg {
+		position: absolute;
+		width: 160rpx;
+		height: 160rpx;
+		right: 0;
+		top: 20rpx;
+		z-index: 4;
+	}
+	.description .right {
+		flex: 1;
+		box-sizing: border-box;
+		overflow: hidden;
+	}
+	.description .right .title {
+		font-size: 24rpx;
+		font-weight: 700;
+		white-space: nowrap;
+		overflow: hidden;
+		text-overflow: ellipsis;
+	}
+	.description .right .tag {
+		box-sizing: border-box;
+		display: flex;
+		font-size: 20rpx;
+		color: #AA6428;
+		margin-top: 14rpx;
+	}
+	.description .right .tag view {
+		background: #FFFBE6;
+		box-sizing: border-box;
+		padding: 2rpx 8rpx;
+		border-radius: 16rpx;
+		margin-right: 6rpx;
+	}
+	.description .right .tag view:last-child {
+		margin-right: 0;
+	}
+	.description .right .play {
+		display: flex;
+		margin-top: 70rpx;
+		align-items: center;
+		font-size: 22rpx;
+		color: #808080;
+	}
+	.description .right .play image {
+		width: 30rpx;
+		height: 30rpx;
+		margin-right: 6rpx;
+	}
+	/* 专辑详情end */
+	/* 简介/节目start */
+	.list {
+		box-sizing: border-box;
+		padding: 30rpx;
+		background: #fff;
+		border-radius: 30rpx;
+		box-shadow: 0px 2px 5px 0px rgba(0,0,0,0.16); 
+		margin-top: 24rpx;
+	}
+	.list .nav {
+		display: flex;
+		line-height: 70rpx;
+		font-size: 28rpx;
+	}
+	.list .nav .item {
+		flex: 1;
+		box-sizing: border-box;
+		text-align: center;
+		position: relative;
+	}
+	.list .nav .item .line {
+		position: absolute;
+		left: 0;
+		bottom: 0;
+		width: 100%;
+		height: 10rpx;
+		background: #F9F9F9;
+		border-radius: 6rpx;
+	}
+	.list .nav .item.active {
+		color: #2AAEC4;
+		font-weight: 700;
+	}
+	.list .nav .item .line.active {
+		background: #BCEDF5;
+	}
+	.list .options {
+		box-sizing: border-box;
+		line-height: 70rpx;
+		display: flex;
+		padding-left: 25rpx;
+		justify-content: space-between;
+		border-bottom: 2rpx dashed #b3b3b3;
+		color: #808080;
+		font-size: 22rpx;
+	}
+	.list .options .item {
+		display: flex;
+		align-items: center;
+	}
+	.list .options .item image {
+		width: 24rpx;
+		height: 24rpx;
+		margin-right: 6rpx;
+	}
+	.list .options .right {
+		display: flex;
+		align-items: center;
+		flex: 1;
+		justify-content: flex-end;
+	}
+	.list .options .right .item {
+		display: flex;
+		align-items: center;
+		margin-right: 24rpx;
+	}
+	.list .options .right .item:last-child {
+		margin-right: 0;
+	}
+	.list .options .right image {
+		width: 30rpx;
+		height: 30rpx;
+	}
+	.list .context {
+		box-sizing: border-box;
+	}
+	.list .context .item {
+		box-sizing: border-box;
+		padding: 20rpx 10rpx 20rpx 25rpx;
+		border-bottom: 2rpx dashed #b3b3b3;
+	}
+	.list .context .item:last-child {
+		border-bottom: 0;
+	}
+	.list .context .introduction {
+		box-sizing: border-box;
+		padding: 25rpx;
+		font-size: 22rpx;
+		line-height: 40rpx;
+	}
+	.list .context .item .detail {
+		display: flex;
+		align-items: center;
+	}
+	.list .context .item .sort {
+		color: #B3B3B3;
+		font-size: 24rpx;
+		flex-shrink: 0;
+		margin-right: 30rpx;
+	}
+	.list .context .item .title {
+		width: 400rpx;
+		font-size: 24rpx;
+	}
+	.list .context .item .icon {
+		margin-left: 120rpx;
+	}
+	.list .context .item .icon image {
+		width: 36rpx;
+		height: 36rpx;
+	}
+	.list .context .item .time {
+		display: flex;
+		align-items: center;
+	}
+	.list .context .item .time .listen {
+		display: flex;
+		font-size: 20rpx;
+		color: #808080;
+		box-sizing: border-box;
+		margin-right: 45rpx;
+		padding-left: 40rpx;
+		margin-top: 4rpx;
+		align-items: center;
+	}
+	.list .context .item .time image {
+		width: 26rpx;
+		height: 26rpx;
+		margin-right: 6rpx;
+	}
 </style>
--- pagesFavourite/XMradio/index.vue
+++ pagesFavourite/XMradio/index.vue
@@ -6,14 +6,13 @@
 			<button @tap="pause">pause</button>
 			<button @tap="stop">stop</button>
 		-->
-		<!--
+		<!-- swiper
 		<view style="position: relative;">
 			<swiper class="carousel" circular autoplay @change="swiperChange">
 				<swiper-item v-for="(item, i) in carouselList" :key="i" class="carousel-item" @tap="toTargetUrl(item)">
 					<image :src="item.banner_cover_url" />
 				</swiper-item>
 			</swiper>
-			
 			<view class="swiper-dots">
 				<text class="num">{{ swiperCurrent + 1 }}</text>
 				<text class="sign">/</text>
@@ -21,50 +20,112 @@
 			</view>
 		</view>
 		-->
-		
+
+		<!-- list
 		<view>
 			<view v-for="(item, i) in albumsList" :key="i" style="height: 200upx; background: #00B7CC; color: #fff; margin-top: 30upx;" @tap="goAlbumDetail(item.id)">
 				<image :src="item.cover_url_middle" style="height: 180upx; width: 180upx;"/>
 				<text>{{item.album_title}}</text>
 			</view>
+		</view> 
+		-->
+		<!-- 专辑列表区域start -->
+		<view class="album">
+			<view class="list">
+				<!-- 专辑分类 -->
+				<view class="album-nav">
+					<scroll-view scroll-x scroll-with-animation :scroll-left="scroll">
+						<block v-for="(item, index) in kindList" :key="index">
+							<view @tap="changeNav(index)" :class="index === currentIndex ? 'active' : ''">
+								<text :class="index === currentIndex ? 'border-color' : ''">{{ item.name }}</text>
+							</view>
+						</block>
+					</scroll-view>
+				</view>
+				<!-- 专辑内容 -->
+				<view class="album-content">
+					<swiper :style="{'height': swiperHeight}" :current="swiperCurrent" @change="swiperChange">
+						<swiper-item v-for="(item, index) in kindList" :key="index">
+							<view class="content-item" v-for="(content, contentIndex) in albumsList" :key="content" @tap="goAlbumDetail(content)">
+								<view class="cover">
+									<image :src="content.cover_url_middle" mode="aspectFit"></image>
+									<view class="play-count">
+										<image :src="$aliImage + 'xmly-icon-play.png'"></image>
+										<text>{{ content.play_count | formatPlayCount }}</text>
+									</view>
+								</view>
+								<view class="title">
+									<image :src="$aliImage + 'xmly-icon-01.png'"></image>
+									<view>{{content.album_title}}</view>
+								</view>
+							</view>
+						</swiper-item>
+					</swiper>
+				</view>
+			</view>
 		</view>
-		
+		<!-- 专辑列表区域end -->
+
+
 	</view>
 </template>
 
 <script>
-	import { initXMLY } from './XM.js'
-	import { 
+	import {
+		initXMLY
+	} from './XM.js'
+	import {
 		XMdeveloper_albumsURL,
 		XMdeveloper_columnsURL,
 		// XMdeveloper_bannersURL,
 		XMdeveloper_categoriesURL,
 		XMalbums_browseURL,
 	} from './XM.js'
-	
+
 	export default {
 		data() {
 			return {
+				$aliImage: this.$aliImage,
 				XMclient: null,
 				XMplayer: null,
 				albumsPage: 1,
 				columnPage: 1,
-				albumsList: [],
-				kindList: [],
-				// swiperCurrent: 0,
+				kindList: [], // 分类栏目数据
+				album_id: '', //分类id
+				albumsList: [], // 分类栏目对应内容数据
+				allAlbumsList: [] , //总收藏内容数据
+				windowWidth: 0, //设备屏幕宽度
+				dataList: [1,2,3,4,5,6,7],
+				scroll: 0, // scroll-view滑动的距离
+				currentIndex: 0, // 当前所在分类的索引
+				swiperCurrent: 0, // 当前swiper所在的滑块
+				itemHeight: 0,
+				swiperHeight: 0
 				// swiperLength: 0,
 				// carouselList: []
 			}
 		},
-		components: {
-
-		},
 		onLoad() {
 			this.init()
+			this.getSystemInfo()
+		},
+		filters: {
+			formatPlayCount(play_count) {
+				let len = String(play_count).length
+				if(len >= 5 && len < 9) {
+					return play_count = (play_count / 10000).toFixed(2) + '万'
+				}
+				else if(len >= 9) {
+					return play_count = (play_count / 100000000).toFixed(2) + '亿'
+				}
+			}
 		},
 		methods: {
 			async init() {
-				const {xmly, player} = await initXMLY()
+				const {
+					xmly,
+					player
+				} = await initXMLY()
 				this.XMclient = xmly
 				this.XMplayer = player
 				// 获取专辑分类
@@ -72,8 +133,9 @@
 				console.log(this.kindList)
 				// 获取收藏专辑
 				this.albumsList = await this.getAlbumsList(this.albumsPage)
+				// 设置swiper高度
+				this.getSwiperHeight('.content-item')
 				console.log(this.albumsList)
-				console.log('ok')
 				// 获取轮播图
 				// this.carouselList = await this.getBanners()
 				// this.swiperLength = this.carouselList.length;
@@ -82,7 +144,7 @@
 				// 获取开发者分类列表
 				const categoriesResult = await this.XMclient.get(XMdeveloper_categoriesURL)
 					.then(res => {
-						if(res.code === 0) {
+						if (res.code === 0) {
 							return res.data.operation_categories
 						}
 					})
@@ -93,19 +155,10 @@
 						finalCategor.push(item)
 					}
 				})
+				this.album_id = finalCategor[0].id
+				console.log(this.album_id)
 				return finalCategor
 			},
-			// async getBanners() {
-			// 	// 获取开发者焦点图
-			// 	const bannersResult = await this.XMclient.get(XMdeveloper_bannersURL)
-			// 		.then(res => {
-			// 			if(res.code === 0) {
-			// 				return res.data.banners
-			// 			}
-			// 			return []
-			// 		})
-			// 	return bannersResult
-			// },
 			async getAlbumsList(page) {
 				// 获取开发者收藏专辑
 				let paramAlbums = {
@@ -114,25 +167,121 @@
 				const albumsResult = await this.XMclient.get(XMdeveloper_albumsURL, paramAlbums)
 					.then(res => {
 						if (res.code === 0) {
-							return res.data.albums
+							this.allAlbumsList = res.data.albums //总专辑数
+							let result = res.data.albums
+							
+							let arr = []
+							// 筛选对应栏目下的专辑
+							result.map(item => {
+								if(item.operation_category.id === this.album_id) {
+									arr.push(item)
+								}
+							})
+							console.log(result)
+							return arr
+							
 						}
 						return []
 					})
 				return albumsResult
 			},
-			async getAlbumsDetail(album_id, page) {
-				// 获取专辑详情信息
-				let paramAlbumDetail = {
-					album_id: album_id,
-					page: page,
-				}
-				const albumsDetailResult = await this.XMclient.get(XMalbums_browseURL, paramAlbumDetail)
-				if (albumsDetailResult.code === 0) {
-					// console.log(albumsDetailResult)
-					return albumsDetailResult
-				}
-				return null
+			
+			// 获取用户设备信息
+			getSystemInfo() {
+				uni.getSystemInfo({
+					success: res => {
+						this.windowWidth = res.windowWidth
+					}
+				})
 			},
+			// 点击分类
+			changeNav(currentIndex) {
+				// 获取当前点击元素的索引
+				const index = currentIndex
+				this.currentIndex = index
+				this.swiperCurrent = index
+				// 获取scroll-view中每个item的宽度
+				const itemWidth = this.windowWidth * 0.23
+				// 需要滚动的临界值(根据自个的需求)
+				let scroll = itemWidth * index - itemWidth * 2;
+				// 滚动的最大距离
+				let maxScroll = (this.kindList.length) * itemWidth
+				if (scroll < 0) {
+					scroll = 0
+				} else if (scroll > maxScroll) {
+					scroll = maxScroll
+				}
+				this.scroll = scroll
+			},
+			// 动态设置swiper高度
+			getSwiperHeight(ele) {
+				setTimeout(() => {
+					const query = uni.createSelectorQuery().in(this);
+					query.select(ele).boundingClientRect(data => {
+					  console.log(data)
+						this.itemHeight = data.height
+						this.swiperHeight = Math.ceil(this.albumsList.length / 3) * this.itemHeight + 30 + 'px'
+						console.log(this.swiperHeight)
+					}).exec();
+				}, 200)
+				
+			},
+			// 监听swiper的改变
+			swiperChange(event) {
+				let index = event.detail.current
+				this.currentIndex = index
+				this.album_id = this.kindList[index].id
+				this.changeNav(index)
+				let arr = []
+				// 筛选对应栏目下的专辑
+				this.allAlbumsList.map(item => {
+					if(item.operation_category.id === this.album_id) {
+						arr.push(item)
+					}
+				})
+				this.albumsList = arr
+				this.swiperHeight = Math.ceil(this.albumsList.length / 3) * this.itemHeight + 48 + 'px'
+				console.log(this.swiperHeight)
+			},
+			// 跳转专辑详情页
+			goAlbumDetail(content) {
+				let id = content.id // 专辑id
+				let tag = content.dimension_tags // 专辑标签
+				let cover_url_middle = content.cover_url_middle // 专辑封面
+				let play_count = content.play_count // 专辑播放次数
+				let album_intro = content.album_intro // 专辑简介
+				let tagList = []
+				if(tag && tag.length > 0) {
+					tag.map(item => {
+						tagList.push(item.name)
+					})
+				}
+				let params = {
+					id: id,
+					tagList: tagList,
+					cover_url_middle: cover_url_middle,
+					play_count: play_count,
+					album_intro: album_intro
+				}
+				uni.setStorageSync('album_detail', JSON.stringify(params))
+				uni.navigateTo({
+					url: '/pagesFavourite/XMradio/albumDetail'
+				})
+			},
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 			play() {
 				console.log('play')
 				this.XMplayer.setPlaylist([28780230])
@@ -146,9 +295,33 @@
 				console.log('stop')
 				this.XMplayer.stop()
 			},
-			async goAlbumDetail(id) {
-				await this.getAlbumsDetail(id, 1)
-			},
+			// async getAlbumsDetail(album_id, page) {
+			// 	// 获取专辑详情信息
+			// 	let paramAlbumDetail = {
+			// 		album_id: album_id,
+			// 		page: page,
+			// 	}
+			// 	const albumsDetailResult = await this.XMclient.get(XMalbums_browseURL, paramAlbumDetail)
+			// 	if (albumsDetailResult.code === 0) {
+			// 		// console.log(albumsDetailResult)
+			// 		return albumsDetailResult
+			// 	}
+			// 	return null
+			// },
+			// async goAlbumDetail(id) {
+			// 	await this.getAlbumsDetail(id, 1)
+			// },
+			// async getBanners() {
+			// 	// 获取开发者焦点图
+			// 	const bannersResult = await this.XMclient.get(XMdeveloper_bannersURL)
+			// 		.then(res => {
+			// 			if(res.code === 0) {
+			// 				return res.data.banners
+			// 			}
+			// 			return []
+			// 		})
+			// 	return bannersResult
+			// },
 			// 监听轮播图发生改变
 			// swiperChange(e) {
 			// 	const index = e.detail.current;
@@ -162,16 +335,127 @@
 		}
 	}
 </script>
-
+<style>
+	page {
+		background: #EBF8FF;
+		padding-top: 24rpx;
+	}
+</style>
 <style scoped>
-.carousel {
+	/* 专辑列表start */
+	.album {
+		box-sizing: border-box;
+		padding: 0 25rpx;
+	}
+	.album .list {
+		box-sizing: border-box;
+		background: #fff;
+		padding: 0 42rpx;
+		box-shadow: 0px 2rpx 5rpx 0px rgba(0, 0, 0, 0.16);
+		border-radius: 30rpx;
+	}
+	.album .album-nav {
+		box-sizing: border-box;
+		padding: 0 8rpx;
+		font-size: 28rpx;
+		line-height: 80rpx;
+	}
+	.album .album-nav scroll-view {
+		box-sizing: border-box;
+		width: 100%;
+		white-space: nowrap;
+	}
+	.album .album-nav scroll-view view {
+		box-sizing: border-box;
+		display: inline-block;
+		width: 23%;
+		text-align: center;
+	}
+	.album .album-nav scroll-view text {
+		border-bottom: 10rpx solid #F9F9F9;
+		padding-bottom: 8rpx;
+	}
+	.album .album-nav scroll-view .active {
+		font-weight: 700;
+		color: #2AAEC4;
+	}
+	.album .album-nav .border-color {
+		border-bottom: 10rpx solid #BCEDF5;
+	}
+	.album .album-content swiper {
+		box-sizing: border-box;
+		padding: 24rpx 0;
+	}
+	.album .album-content swiper-item {
+		box-sizing: border-box;
+	}
+	.album .album-content .content-item {
+		width: 180rpx;
+		box-sizing: border-box;
+		margin-right: 38rpx;
+		margin-bottom: 26rpx;
+		display: inline-block;
+	}
+	.album .album-content .content-item:nth-child(3n) {
+		margin-right: 0;
+	}
+	.album .album-content swiper-item .cover {
+		box-sizing: border-box;
+		position: relative;
+	}
+	.album .album-content .play-count {
+		position: absolute;
+		left: 0;
+		bottom: 0;
+		font-size: 18rpx;
+		color: #fff;
+		background: rgba(128,128,128,0.64);
+		display: flex;
+		align-items: center;
+		padding: 4rpx 12rpx;
+		border-bottom-left-radius: 16rpx;
+		border-top-right-radius: 16rpx;
+	}
+	
+	.album .album-content swiper-item .cover image {
+		width: 180rpx;
+		height: 180rpx;
+		display: block;
+	}
+	.album .album-content swiper-item .play-count image {
+		width: 16rpx;
+		height: 16rpx;
+		flex-shrink: 0;
+		margin-right: 8rpx;
+	}
+	.album .album-content swiper-item .title {
+		font-size: 24rpx;
+		margin-top: 12rpx;
+		position: relative;
+		text-indent: 40rpx;
+		display: -webkit-box;
+		-webkit-line-clamp: 2;
+		-webkit-box-orient: vertical;
+		overflow: hidden;
+	}
+	.album .album-content swiper-item .title image {
+		width: 30rpx;
+		height: 30rpx;
+		position: absolute;
+		left: 0;
+		top: 0;
+	}
+	/* 专辑列表end */
+
+
+
+
+	/* .carousel {
 	width: 100%;
 	height: 450upx;
 }
 .carousel .carousel-item {
-	/* width: 98%!important; */
 	height: 100%;
-	/* padding: 0 28upx; */
 	overflow: hidden;
 }
 .carousel-item image {
@@ -206,5 +490,5 @@
 	font-size: 12upx;
 	color: #fff;
 	transform: translateX(-50%);
-}
+} */
 </style>
